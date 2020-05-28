39e5ddbbde46f55493530eaac925dbeebe569f51
zac-evans <zacisadeveloper@gmail.com>
2020-05-28 19:50:03 -0400

    fix

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,6 @@
 function myFunc(name, otherName) {
-    console.log("Hello," + ' ' name + "!");
-    console.log("And" + ' ' + otherName + "!");
+    console.log("Hello," + " " name + "!");
+    console.log("And" + " " + otherName + "!");
 }
 
 myFunc("Adam","Krissy");
